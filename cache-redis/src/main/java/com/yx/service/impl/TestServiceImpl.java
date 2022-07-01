4217d87fdc3293885ff003886c19599f244e01f3
DYXing <18211050021@163.com>
2022-07-02 00:21:23 +0800

    测试另外一台机器提交

---
+++ cache-redis/src/main/java/com/yx/service/impl/TestServiceImpl.java
@@ -0,0 +1,14 @@
+package com.yx.service.impl;
+
+import com.yx.service.TestService;
+
+/**
+ * Description:TODO
+ *
+ * @author dulj
+ * @date 2022/7/2 0:20
+ * @since V1.0
+ * Copyright www.enbrands.com
+ */
+public class TestServiceImpl implements TestService {
+}
